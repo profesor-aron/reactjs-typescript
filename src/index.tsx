4d8f73165d7903284b9653f6b4ce45642fa87b52
Aron Herrera <aronherrera2009@gmail.com>
2018-01-28 18:35:15 +0100

    Add Login Page

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,42 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+import {Login} from './modules/Login'
+
 class App extends Component {
   render() {
+    const data = {
+      appName: 'React JS TypeScript',
+      devepoler: 'Aron HERRERA PONTE'
+    }
+
+    let allData = {
+      ...data,
+      twitterAccount: '@aehp20'
+    }
+
+    const displayData = () => {
+      const convertObjToArray = () => {
+        const items = []
+        for (const key in allData) {
+          items.push({key, value: allData[key]})
+        }
+        return items
+      }
+      return (
+        <ul>
+          {
+            convertObjToArray().map((item, index) => <li key={index}>{item.key} : {item.value}</li>)
+          }
+        </ul>
+      )
+    }
+
     return (
       <div>
         <h1>Arriba siempre arriba hasta las estrellas!</h1>
+        {displayData()}
+        <Login/>
       </div>
     )
   }
