5ca5cdcb910beea587710f6a883d8c01ae5a097b
Aron Herrera <aronherrera2009@gmail.com>
2018-02-03 20:33:51 +0100

    udpate feature2

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -4,7 +4,7 @@ export class Login extends Component {
   render() {
     return (
       <div>
-        Feature 1 Login Page in contruction, enjoy it soon!!
+        Login Page in contruction, enjoy it soon!
       </div>
     )
   }
