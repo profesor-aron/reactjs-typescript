72d03b9465970ef97d2d7630888f5237cf691077
Aron Herrera <aronherrera2009@gmail.com>
2018-02-04 13:12:47 +0100

    Update F2

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -4,7 +4,7 @@ export class Login extends Component {
   render() {
     return (
       <div>
-        F1 Login Page in contruction, enjoy it soon!
+        Login Page in contruction, enjoy it soon!
       </div>
     )
   }
